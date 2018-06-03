70c57c3fe346364981d68b4cfb80e9948b954e04
agalusha <aquila.galusha@gmail.com>
2018-06-03 00:11:11 -0500

    Starting php tests
    this only includes the header in the index.php file and the includes/header.php insert

---
+++ index.php
@@ -0,0 +1,170 @@
+<?php include('includes/header.php'); ?>
+
+                    <div id="diywebNavigation">
+                        <div class="webnavigation">
+                            <ul id="mainNav1" class="mainNav1">
+                                <li class="navTopItemGroup_1" style="height:61.5px;">
+                                    <a data-page-id="368002" href="/" style="padding:0;" class="current level_1">
+                                        <span>
+                                            <img src="images/home.png" style="max-width: 20px; padding: 21px" alt=""/>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="navTopItemGroup_2">
+                                    <a data-page-id="368279" href="about-steve" class="level_1">
+                                        <span>
+                                            About
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="navTopItemGroup_3">
+                                    <a data-page-id="368304" href="steve-in-action" class="level_1">
+                                        <span>
+                                            In Action
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="navTopItemGroup_4">
+                                    <a data-page-id="368307" href="upcoming-shows-and-other-news" class="level_1">
+                                        <span>
+                                            Upcoming
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="navTopItemGroup_5">
+                                    <a data-page-id="368305" href="laugh-with-steve" class="level_1">
+                                        <span>
+                                            Laugh
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="navTopItemGroup_6">
+                                    <a data-page-id="368325" href="credentials" class="level_1">
+                                        <span>
+                                            Credentials
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="navTopItemGroup_7">
+                                    <a data-page-id="368284" href="reviews" class="level_1">
+                                        <span>
+                                            Reviews
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="navTopItemGroup_8">
+                                    <a data-page-id="368306" href="contact-steve" class="level_1">
+                                        <span>
+                                            Contact
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="navTopItemGroup_9">
+                                    <a data-page-id="378642" href="links" class="level_1">
+                                        <span>
+                                            Links
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="navTopItemGroup_10">
+                                    <a data-page-id="754949" href="featured-artists" class="level_1">
+                                        <span>
+                                            Featured
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div id="diywebMainContent">
+                        <div id="diywebSidebar">
+                            <div id="diywebSubContent">
+                                <div id="matrix_448836" class="sortable-matrix" data-matrixId="448836">
+                                    <div class="n module-type-imageSubtitle diyfeLiveArea ">
+                                        <div class="clearover imageSubtitle" id="imageSubtitle-10578115">
+                                            <div class="align-container align-left" style="max-width: 170px">
+                                                <a class="imagewrapper" href="images/profile.jpg" rel="lightbox[10578115]">
+                                                    <img src="images/profile.jpg" alt="" style="max-width: 266px; height:auto; padding-left: 25px;padding-right: 25px"/>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="diywebMainContentCenter">
+                        <div id="content_area">
+                            <div id="matrix_448837" class="sortable-matrix" data-matrixId="448837">
+                                <div class="n module-type-header diyfeLiveArea "> 
+                                    <h1>
+                                        <br>
+                                        <span class="diyfeDecoration">
+                                            Welcome
+                                        </span>
+                                    </h1> 
+                                </div>
+                                <div class="n module-type-hr diyfeLiveArea "> 
+                                    <div style="padding: 0px 0px">
+                                        <div class="hr"></div>
+                                    </div>
+                                </div>
+                                <div class="n module-type-text diyfeLiveArea "> 
+                                    <h3><span style="color:#ffffff">
+                                        Steve Hensley is an actor/comedian, based in Chicago, IL. He has been performing since around the age of six, and has performed in various settings across the United States.  In addition to performing his routines, Steve has taught comedy improv workshops to hundreds of youth of varying skill levels.
+                                        </span>
+                                    </h3>
+                                    <h3> </h3>
+                                    <h3>
+                                        Steve brings a fresh approach to his comedy through unexpected twists that leave the audience wondering what he will come up with next.
+                                    </h3>
+                                    <h3> </h3>
+                                    <h3>
+                                        Steve is a graduate of Graceland University, where he studied Theatre, Film, and Education.
+                                    </h3>
+                                </div>
+                                <div class="n module-type-hr diyfeLiveArea "> 
+                                    <div style="padding: 0px 0px">
+                                        <div class="hr"></div>
+                                    </div>
+                                </div>
+                                <div class="n module-type-text diyfeLiveArea "> 
+                                    <h1>
+                                        "I like to make people laugh who are normally serious"
+                                    </h1>
+                                    <h1 style="text-align: right;">-Jimmy Fallon</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <br clear="all"/>
+                </div>
+            </div>
+
+            
+            <div id="diywebFooterWrap">
+                <div id="diywebFooter">
+                    <div id="diywebFooterCenter" class="center">
+                        <div id="contentfooter">
+                            <div class="leftrow">
+                                <br/>
+                                &copy 
+                                <script>
+                                    var d = new Date();
+                                    var n = d.getFullYear();
+                                    document.write(n)
+                                </script> &nbsp; - &nbsp; Steve Hensley
+                            </div>
+                        
+                            <div class="rightrow"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </body>
+</html>
