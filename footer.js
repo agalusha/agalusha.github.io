af6b0149dbc4c63de1844ff0338090a19d15d6f1
agalusha <aquila.galusha@gmail.com>
2018-06-17 21:00:25 -0500

    Footer Icons

--- footer.js
+++ footer.js
@@ -4,7 +4,32 @@ document.write('\
         <div id="diywebFooterCenter" class="center">\
             <div id="contentfooter">\
                 <div class="leftrow">\
-                    <br/>\
+                    <div class="footIconHolder">\
+                        <a href="https://www.facebook.com/steve.hensley.165" target="_blank" title="Facebook">\
+                            <img class="footIcons" src="images/icons/faceCirc.svg" />\
+                        </a>\
+                    </div>\
+                    <div class="footIconHolder">\
+                        <a href="https://www.instagram.com/hensley8/" target="_blank" title="Instagram">\
+                            <img class="footIcons" src="images/icons/instCirc.svg" />\
+                        </a>\
+                    </div>\
+                    <div class="footIconHolder">\
+                        <a href="https://www.linkedin.com/in/steve-hensley-0b1917137/" target="_blank" title="LinkedIn">\
+                            <img class="footIcons" src="images/icons/linkedCirc.svg" />\
+                        </a>\
+                    </div>\
+                    <div class="footIconHolder">\
+                        <a href="https://www.youtube.com/TheHensley8" target="_blank" title="YouTube">\
+                            <img class="footIcons" src="images/icons/tubeCirc.svg" />\
+                        </a>\
+                    </div>\
+                    <div class="footIconHolder">\
+                        <a href="https://twitter.com/shensley8" target="_blank" title="Twitter">\
+                            <img class="footIcons" src="images/icons/twitterCirc.svg" />\
+                        </a>\
+                    </div>\
+                    <br/><br/>\
                     &copy\
                     <script>\
                         var d = new Date();\
